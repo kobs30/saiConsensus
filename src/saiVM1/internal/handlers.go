e360799c708f0f40b0fd05bddb50230de25f2746
Kobs <sergeik30@gmail.com>
2023-01-25 08:25:31 +0100

    VM few functions added, saiStrorage connection hack

--- src/saiVM1/internal/handlers.go
+++ src/saiVM1/internal/handlers.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/robertkrimen/otto"
+	"strings"
+
 	// https://go.libhunt.com/otto-alternatives    other languages alternatives
 	"github.com/saiset-co/saiService"
 	"go.mongodb.org/mongo-driver/bson"
@@ -47,6 +49,7 @@ func (is InternalService) execute(data interface{}) interface{} {
 	var Validators []map[string]bool
 	var Distribution []map[string]int64
 	var CustomFld []map[string]string
+	var Fee float64
 	var request VMrequest
 	fmt.Println("REQUEST:::::::", data)
 
@@ -66,7 +69,38 @@ func (is InternalService) execute(data interface{}) interface{} {
 		fmt.Println("datERROR", err)
 		fmt.Println("REQUEST CONV::ERROR", err)
 	}
+	currentContract := ""
+	switch vmScript.Method {
+	case "execute":
+		{
+		}
+	case "save":
+		// Curl example:
+		// curl --location --request GET 'http://185.229.119.188:8018' \
+		//--header 'Content-Type: text/plain' \
+		//--data-raw '{"method":"get-tx","data":"{\"method\": \"execute\", \"data\": \"function hello(name) { return '\'' Hello '\''+ name}; hello('\''world'\'')  ;\"}"}'
+		saveScriptData := make(map[string]string)
+		saveScriptData[request.Tx.MessageHash] = "saved"
+		CustomFld = append(CustomFld, saveScriptData)
+		return bson.M{"vm_processed": true, "vm_result": true, "vm_response": bson.M{"callNumber": counter, "D": Distribution, "V": Validators, "C": CustomFld, "F": Fee}}
+	case "run":
+		// Curl example:
+		// curl --location --request GET 'http://185.229.119.188:8018' \
+		//--header 'Content-Type: text/plain' \
+		//--data-raw '{"method":"get-tx","data":"{\"method\": \"run\", \"data\": \"f71aec98f3fab3f8ab3af2131465173194fbe45fcb82e07f49b98ef18a10e03b.hello('\''world'\'')  ;\"}"}'
+		parts := strings.Split(vmScript.Script, ".")
+		Contract := parts[0]
+		Function := parts[1]
+		currentContract = Contract
+		runScript, _ := getMessageByHash(Contract, is)
+		logRecord := make(map[string]string)
+		logRecord[Contract] = "contract was run with " + Function
+		CustomFld = append(CustomFld, logRecord)
+		vmScript.Script = runScript + " " + Function
+	}
+
 	theSender := request.Tx.SenderAddress // request.Tx.Sender
+	theScriptHash := request.Tx.MessageHash
 	fmt.Println("XXXXX", vmScript.Script)
 	if vmScript.Script == "fly me to the moon" && request.Block == 1 {
 		fmt.Println("L51")
@@ -128,41 +162,14 @@ func (is InternalService) execute(data interface{}) interface{} {
 
 	vm.Set("getBalance", func(call otto.FunctionCall) otto.Value {
 		Wallet, _ := call.Argument(0).ToString()
-		_, blockhainData := is.Storage.Get("MessagesPool", bson.M{"vm_response.D": bson.M{"$elemMatch": bson.M{Wallet: bson.M{"$exists": 1}}}}, bson.M{})
-		fmt.Println("blockhainData", string(blockhainData))
-		var jsonBlockchainData JSONRESP
-		err := json.Unmarshal(blockhainData, &jsonBlockchainData)
+		WalletBalance, err := getBalance(Wallet, is)
 		if err != nil {
-			fmt.Println("datERROR", err)
 			res, _ := vm.ToValue(false)
 			return res
+		} else {
+			res, _ := vm.ToValue(WalletBalance)
+			return res
 		}
-
-		// Unmarshal C through reflekt
-		/*
-			CType := reflect.ValueOf(jsonBlockchainData.Result[0].VMResponse.C)
-			if CType.Kind() == reflect.Map {
-				var CustomFld map[string]string
-				json.Unmarshal(jsonBlockchainData.Result[0].VMResponse.C, &CustomFld)
-				fmt.Println(CustomFld)
-			}
-		*/
-
-		fmt.Println("dat", jsonBlockchainData)
-		fmt.Println("datVMResponse PUPUPUP", jsonBlockchainData.Result[0].VMResponse.D[0][Wallet])
-		// {"vm_response.D":{$elemMatch: {"1FTGGrgfHTsgHsw0f8Hff8": {$exists:true} } } }
-		var WalletBalance int64
-		for _, el := range jsonBlockchainData.Result {
-			for _, d := range el.VMResponse.D {
-				balance, ok := d[Wallet]
-				if ok {
-					fmt.Println("-------", d[Wallet])
-					WalletBalance += balance
-				}
-			}
-		}
-		res, _ := vm.ToValue(WalletBalance)
-		return res
 	})
 
 	vm.Set("addValidator", func(call otto.FunctionCall) otto.Value {
@@ -196,6 +203,45 @@ func (is InternalService) execute(data interface{}) interface{} {
 		return otto.Value{}
 	})
 
+	vm.Set("transfer", func(call otto.FunctionCall) otto.Value {
+		to, _ := call.Argument(0).ToString()
+		amount, _ := call.Argument(1).ToInteger()
+		WalletBalance, _ := getBalance(request.Tx.SenderAddress, is)
+		if (WalletBalance - amount) > 0 {
+			balance := make(map[string]int64)
+			balance[request.Tx.SenderAddress] = int64(0 - amount)
+			Distribution = append(Distribution, balance)
+			balance = make(map[string]int64)
+			balance[to] = int64(amount)
+			Distribution = append(Distribution, balance)
+			return otto.TrueValue()
+		} else {
+			return otto.FalseValue()
+		}
+	})
+	vm.Set("transferFromContract", func(call otto.FunctionCall) otto.Value {
+		if currentContract == "" {
+			return otto.FalseValue()
+		}
+		to, _ := call.Argument(0).ToString()
+		amount, _ := call.Argument(1).ToInteger()
+		contractBalance, _ := getBalance(currentContract, is)
+		if (contractBalance - amount) > 0 {
+			balance := make(map[string]int64)
+			balance[request.Tx.SenderAddress] = int64(0 - amount)
+			Distribution = append(Distribution, balance)
+			balance = make(map[string]int64)
+			balance[to] = int64(amount)
+			Distribution = append(Distribution, balance)
+			return otto.TrueValue()
+		} else {
+			return otto.FalseValue()
+		}
+	})
+
+	vm.Set("currentFee", func(call otto.FunctionCall) otto.Value {
+		return otto.Value{}
+	})
 	//vm.SetTimeout(time.Second)
 	result, err := vm.Run(vmScript.Script)
 	if err != nil {
@@ -205,10 +251,66 @@ func (is InternalService) execute(data interface{}) interface{} {
 
 	fmt.Println(result)
 	CustomFldElement := make(map[string]string)
-	CustomFldElement["theScriptOrTxHash"], _ = result.ToString()
+	CustomFldElement[theScriptHash], _ = result.ToString()
 	CustomFld = append(CustomFld, CustomFldElement)
 	fmt.Println("RETURN ::::: ", bson.M{"vm_processed": true, "vm_result": true, "vm_response": bson.M{"callNumber": counter, "D": Distribution, "V": Validators, "C": CustomFld}})
-	return bson.M{"vm_processed": true, "vm_result": true, "vm_response": bson.M{"callNumber": counter, "D": Distribution, "V": Validators, "C": CustomFld}}
+	return bson.M{"vm_processed": true, "vm_result": true, "vm_response": bson.M{"callNumber": counter, "D": Distribution, "V": Validators, "C": CustomFld, "F": Fee}}
+}
+
+func getBalance(Wallet string, is InternalService) (int64, error) {
+	_, blockhainData := is.Storage.Get("MessagesPool", bson.M{"vm_response.D": bson.M{"$elemMatch": bson.M{Wallet: bson.M{"$exists": 1}}}}, bson.M{})
+	fmt.Println("blockhainData", string(blockhainData))
+	var jsonBlockchainData JSONRESP
+	err := json.Unmarshal(blockhainData, &jsonBlockchainData)
+	if err != nil {
+		fmt.Println("datERROR", err)
+		return 0, err
+	}
+
+	// Unmarshal C through reflekt
+	/*
+		CType := reflect.ValueOf(jsonBlockchainData.Result[0].VMResponse.C)
+		if CType.Kind() == reflect.Map {
+			var CustomFld map[string]string
+			json.Unmarshal(jsonBlockchainData.Result[0].VMResponse.C, &CustomFld)
+			fmt.Println(CustomFld)
+		}
+	*/
+
+	fmt.Println("dat", jsonBlockchainData)
+	fmt.Println("datVMResponse PUPUPUP", jsonBlockchainData.Result[0].VMResponse.D[0][Wallet])
+	// {"vm_response.D":{$elemMatch: {"1FTGGrgfHTsgHsw0f8Hff8": {$exists:true} } } }
+	var WalletBalance int64
+	for _, el := range jsonBlockchainData.Result {
+		for _, d := range el.VMResponse.D {
+			balance, ok := d[Wallet]
+			if ok {
+				fmt.Println("-------", d[Wallet])
+				WalletBalance += balance
+			}
+		}
+	}
+	return WalletBalance, nil
+}
+
+func getMessageByHash(MessageHash string, is InternalService) (string, error) {
+	// {"collection":"MessagesPool", "select": {"block_number": {"$exists":true}, "message_hash": "d8e7f63670d4e8ff434d031de226609bc1cb64eeae3ee496553f4cabb22a8c64","vm_processed": true,"vm_result": true}, "options": {} }
+	_, blockhainData := is.Storage.Get("MessagesPool", bson.M{"block_number": bson.M{"$exists": true}, "message_hash": MessageHash, "vm_processed": true, "vm_result": true}, bson.M{})
+	fmt.Println("blockhainData", string(blockhainData))
+	var jsonBlockchainData JSONRESP
+	err := json.Unmarshal(blockhainData, &jsonBlockchainData)
+	if err != nil {
+		fmt.Println("datERROR", err)
+		return "", err
+	}
+	message := jsonBlockchainData.Result[0].Message.Message
+	var vmScript VMscript
+	err = json.Unmarshal([]byte(message), &vmScript)
+	if err != nil {
+		return "", err
+	} else {
+		return vmScript.Script, nil
+	}
 }
 
 func getValidatorWalletLicence(wallet string) bool {
@@ -243,7 +345,7 @@ type Result struct {
 	VMResponse  struct {
 		C          json.RawMessage    `json:"C"`
 		D          []map[string]int64 `json:"D"`
-		V          []string           `json:"V"`
+		V          []map[string]bool  `json:"V"`
 		CallNumber int                `json:"callNumber"`
 	} `json:"vm_response"`
 	VMResult bool  `json:"vm_result"`

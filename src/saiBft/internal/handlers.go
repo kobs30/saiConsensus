0750e93c493cb2bff81953b1245b3b401ec1ba94
Dmitriy Milevskiy <natapp@inbox.ru>
2023-01-25 20:57:49 +0200

    fix tx cli/http handler

--- src/saiBft/internal/handlers.go
+++ src/saiBft/internal/handlers.go
@@ -61,8 +61,27 @@ var HandleTxFromCli = saiService.HandlerElement{
 	Name:        "tx",
 	Description: "handle tx message",
 	Function: func(data interface{}) (interface{}, error) {
+
+		argsStr := make([]string, 0)
 		txStruct := models.TxFromHandler{
 			IsFromCli: false,
+		}
+		switch args := data.(type) {
+		case []string:
+			argsStr = args
+			Service.GlobalService.Logger.Debug("got message from cli", zap.Strings("data", argsStr))
+			txStruct.IsFromCli = true
+		case []interface{}:
+			for _, iface := range args {
+				strArg, ok := iface.(string)
+				if !ok {
+					return nil, fmt.Errorf("wrong argument type in tx data, argument : [%s],type  :[%s]", iface, reflect.TypeOf(data))
+				}
+				argsStr = append(argsStr, strArg)
+			}
+			Service.GlobalService.Logger.Debug("got message from http", zap.Strings("data", argsStr))
+		default:
+			return nil, fmt.Errorf("wrong type for args in cli tx method, current type :%s", reflect.TypeOf(data))
 		}
 
 		transactionMessage, err := models.CreateTxMsg(Service.BTCkeys, Service.GlobalService.GetConfig(SaiBTCaddress, "").String(), data.(string))

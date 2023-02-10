abfaacc301097c7a4c4573332a1231086539dd83
Dmitriy Milevskiy <natapp@inbox.ru>
2023-02-10 20:36:44 +0200

    log before return

--- src/saiBft/internal/sync.go
+++ src/saiBft/internal/sync.go
@@ -39,14 +39,16 @@ func (s *InternalService) syncSleep(msg *models.ConsensusMessage) int {
 		}
 	}
 	if countForLagging == totalKeysLen { // если мы отстаем более чем на 1 раунд от любой ноды, независимо от веса -> round = round + 2
+		s.GlobalService.Logger.Debug("process -sync - lagging at all nodes")
 		return 2
 	}
 
 	for k, v := range s.SyncConsensusMap {
 		if k.BlockNumber == msg.BlockNumber && v >= currentCount && msg.Round < k.Round { // если мы отстаем от любого раунда, у которого вес больше или равен нашему -> round = round + 2
+			s.GlobalService.Logger.Debug("process -sync - lagging at special node")
 			return 2
 		} else if k.BlockNumber == msg.BlockNumber && msg.Round > k.Round && v > currentCount { // если мы торопимся и имеем меньший раунд с большим весом чем у нас, т.е. 3й раунд с весом 3, то round = round + 0
-
+			s.GlobalService.Logger.Debug("process -sync - we are hurrying")
 			return 0
 		}
 	}
